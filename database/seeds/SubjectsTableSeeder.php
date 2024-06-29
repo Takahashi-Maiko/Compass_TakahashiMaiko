fdb16615ae73f6520ddefe9de83b61d0b0baacd7
DESKTOP-BK5N0B9\81905 <19960527maiko@gmail.com>
2024-06-29 18:35:30 +0900

    初期データの登録出来ました。

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -13,5 +13,27 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+        // insert()を使ってデータ追加する場合明確的に日付データを追加しないとNULLになってしまうので'created_at'=>now() を記述する。
+        DB::table('subjects')->insert([
+            [
+                'subject' => '国語',
+                'created_at'=>now(),
+            ]
+            ]);
+
+                    DB::table('subjects')->insert([
+            [
+                'subject' => '数学',
+                'created_at'=>now(),
+            ]
+            ]);
+
+                    DB::table('subjects')->insert([
+            [
+                'subject' => '英語',
+                'created_at'=>now(),
+            ]
+            ]);
+
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+         //初期ユーザーの登録(6/29)
+        DB::table('users')->insert([
+            [
+                'over_name' => '高橋',
+                'under_name' => '真依子',
+                'over_name_kana' => 'タカハシ',
+                'under_name_kana' => 'マイコ',
+                'mail_address' => 'maiko@user.com',
+                'sex' => '2',
+                'birth_day' => '19960527',
+                'role' => '1',
+                'password' => bcrypt('maiko0527')
+            ]
+            ]);
+
 
     }
 }
