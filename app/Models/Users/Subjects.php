d0254adc9ffb9c29cc8caf4d47aa233e21cf877f
DESKTOP-BK5N0B9\81905 <19960527maiko@gmail.com>
2024-07-06 19:08:10 +0900

    usersテーブルとsubjectsテーブルのリレーション完了しました。

--- app/Models/Users/Subjects.php
+++ app/Models/Users/Subjects.php
@@ -16,6 +16,13 @@ class Subjects extends Model
     ];
 
     public function users(){
-        return $this->belongsTo('App\Models\Users\User');// リレーションの定義(usersテーブルとの)
+        return $this->belongsToMany('App\Models\Users\User', 'subject_users', 'subject_id', 'user_id');
+    //　↑↑(2024/7/6)usersテーブルとsubjectsテーブルのリレーション(多対多)
+    //多対多のリレーションなのでbelongsToManyをメソッドとして使用。
+    //第一引数＝使用する相手のモデル
+    //第二引数＝使用するテーブル名
+    //第三引数＝リレーションを定義しているモデルの外部キー名
+    //第四引数＝結合するモデルの外部キー名
+
     }
 }
--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -67,7 +67,14 @@ class User extends Authenticatable
     }
 
     public function subjects(){
-        return $this->hasMany('App\Models\Users\Subjects');// リレーションの定義(subjectsテーブルとの)
+        return $this->belongsToMany('App\Models\Users\Subjects', 'subject_users', 'user_id', 'subject_id');
+    //　↑↑(2024/7/6)usersテーブルとsubjectsテーブルのリレーション(多対多)
+    //多対多のリレーションなのでbelongsToManyをメソッドとして使用。
+    //第一引数＝使用する相手のモデル
+    //第二引数＝使用するテーブル名
+    //第三引数＝リレーションを定義しているモデルの外部キー名
+    //第四引数＝結合するモデルの外部キー名
+
     }
 
     // いいねしているかどうか
